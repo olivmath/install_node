a9eecbcfd890da68e2cd6a972a6801285e8c0bdc
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2021-06-05 00:13:59 -0300

    Update pre_req.sh

--- pre_req.sh
+++ pre_req.sh
@@ -2,3 +2,10 @@ apt-get update -y
 
 # Install java: 8 or 11
 apt-get install openjdk-8-jre -y
+
+# Install SBT
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
+apt-get update
+apt-get install sbt
